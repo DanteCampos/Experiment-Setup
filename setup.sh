c962c46dbe3c5619b8117435a09c270ab94126f4
Daniel "Dante" Campos da Silva <41027230+DanteCampos@users.noreply.github.com>
2022-07-17 23:21:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,33 +2,18 @@
 
 mkdir experiment
 cd experiment
-
 apt-get update
 apt-get install git
-
 apt-get update
-apt-get install \
-ca-certificates \
-curl \
-gnupg \
-lsb-release
-
+apt-get install ca-certificates curl gnupg lsb-release
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
 apt-get install docker-compose
-
 git clone https://github.com/free5gc/gtp5g
-
 cd gtp5g
 make clean && make
 make install
 cd ..
-
