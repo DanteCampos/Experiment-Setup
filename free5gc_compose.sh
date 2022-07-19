cc4939cefe191ad1fc0c5784ba7296d54609ed5b
Daniel "Dante" Campos da Silva <41027230+DanteCampos@users.noreply.github.com>
2022-07-18 21:33:46 -0300

    Update and rename setup5.sh to free5gc_compose.sh

---
+++ free5gc_compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Instalando o Free5GC-Compose
+sudo git clone https://github.com/free5gc/free5gc-compose.git
+cd free5gc-compose
+sudo make base
+sudo docker-compose build
+sudo docker-compose up -d
+cd ..
