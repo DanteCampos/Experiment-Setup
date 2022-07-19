2851e0866d5e27b320cf2857efeb13f1325d0491
Daniel "Dante" Campos da Silva <41027230+DanteCampos@users.noreply.github.com>
2022-07-19 13:28:13 -0300

    Update and rename aaa.sh to docker_post_install.sh

--- aaa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo bash basic_settings.sh
-sudo bash docker engine.sh
-sudo bash docker_compose.sh
-sudo bash gtp5g.sh
-sudo bash free5gc_compose.sh
-sudo bash my5g_rantester_docker.sh
